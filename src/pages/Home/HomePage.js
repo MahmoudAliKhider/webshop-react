cdcda3344f2b5e522cf2316159652bd05730e6e2
MahmoudAliKhider <mahmoudkhider37@gmail.com>
2023-10-08 03:30:51 +0300

    create acategory card & connected to homecategory

--- src/pages/Home/HomePage.js
+++ src/pages/Home/HomePage.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import NavBarLogin from '../../components/Uitily/NavBarLogin'
 import Slider from '../../components/Home/Slider'
+import HomeCategory from '../../components/Home/HomeCategory'
 
 const HomePage = () => {
     return (
         <div >
             <NavBarLogin />
             <Slider />
+            <HomeCategory/>
         </div>
     )
 }
