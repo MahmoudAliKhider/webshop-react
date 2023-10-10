24682ea35bc4377b27823718fe2a696a8ecae6ee
MahmoudAliKhider <mahmoudkhider37@gmail.com>
2023-10-10 04:13:41 +0300

    add favorite products Page

--- src/App.js
+++ src/App.js
@@ -22,6 +22,7 @@ import AdminAddSubCategoryPage from "./pages/Admin/AdminAddSubCategoryPage";
 import AdminAddProductsPage from "./pages/Admin/AdminAddProductsPage";
 
 import UserAllOrdersPage from "./pages/User/UserAllOrdersPage";
+import UserFavoriteProductsPage from "./pages/User/UserFavoriteProductsPage";
 
 
 function App() {
@@ -49,6 +50,7 @@ function App() {
           <Route path="admin/addproduct" element={ <AdminAddProductsPage />} />
 
           <Route path="user/allorders" element={ <UserAllOrdersPage />} />
+          <Route path="user/favoriteproducts" element={ <UserFavoriteProductsPage />} />
         </Routes>
       </BrowserRouter>
       <Footer />
