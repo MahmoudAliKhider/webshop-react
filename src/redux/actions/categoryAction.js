bc3ad1406ffd8ad6f504d9619bc5be35b551986d
MahmoudAliKhider <mahmoudkhider37@gmail.com>
2023-10-12 02:30:02 +0300

    useGetData => to get url data

--- src/redux/actions/categoryAction.js
+++ src/redux/actions/categoryAction.js
@@ -1,12 +1,12 @@
 import { GET_ALL_CATEGORY, GET_ERROR } from '../type';
-import baseUrl from '../../Api/baseURL';
+import useGetData from '../../hooks/useGetData';
 
 export const getAllCategory = () => async (dispatch) => {
     try {
-        const res = await baseUrl.get('/categories');
+        const response = await useGetData('/categories')
         dispatch({
             type: GET_ALL_CATEGORY,
-            payload: res.data
+            payload: response.data
         })
     } catch (error) {
         dispatch({
