2a5d2d6da2e810902d80927cc52a2d682b7dd012
MahmoudAliKhider <mahmoudkhider37@gmail.com>
2023-10-19 01:55:27 +0300

    remove product

--- src/components/Admin/AdminAllProductsCard.js
+++ src/components/Admin/AdminAllProductsCard.js
@@ -1,10 +1,39 @@
-import React from 'react'
-import { Col, Card, Row } from 'react-bootstrap'
+import React, { useState } from 'react'
+import { Col, Card, Row, Modal, Button } from 'react-bootstrap'
+import { useDispatch } from 'react-redux';
 import { Link } from 'react-router-dom'
-import prod1 from '../../images/prod1.png'
-const AdminAllProductsCard = ({item}) => {
+import { deleteProducts } from "../../redux/actions/productsAction"
+const AdminAllProductsCard = ({ item }) => {
+    const [show, setShow] = useState(false);
+    const handleClose = () => setShow(false);
+    const handleShow = () => setShow(true);
+
+    const dispatch = useDispatch();
+
+    const handelDelete = async () => {
+
+        await dispatch(deleteProducts(item._id))
+        setShow(false);
+        window.location.reload();
+    }
     return (
         <Col xs="12" sm="6" md="5" lg="4" className="d-flex">
+
+            <Modal show={show} onHide={handleClose}>
+                <Modal.Header >
+                    <Modal.Title> <div className='font'>تاكيد الحذف</div></Modal.Title>
+                </Modal.Header>
+                <Modal.Body><div className='font'>هل انتا متاكد من عملية الحذف للمنتج</div></Modal.Body>
+                <Modal.Footer>
+                    <Button className='font' variant="success" onClick={handleClose}>
+                        تراجع
+                    </Button>
+                    <Button className='font' variant="dark" onClick={handelDelete}>
+                        حذف
+                    </Button>
+                </Modal.Footer>
+            </Modal>
+
             <Card
                 className="my-2"
                 style={{
@@ -16,16 +45,16 @@ const AdminAllProductsCard = ({item}) => {
                 }}>
                 <Row className="d-flex justify-content-center px-2">
                     <Col className=" d-flex justify-content-between">
-                        <div className="d-inline item-delete-edit">ازاله</div>
+                        <div onClick={handleShow} className="d-inline item-delete-edit">ازاله</div>
                         <div className="d-inline item-delete-edit">تعديل</div>
                     </Col>
                 </Row>
-                <Link to="/products/:id" style={{ textDecoration: "none" }}>
+                <Link to={`/product/${item._id}`} style={{ textDecoration: "none" }}>
                     <Card.Img style={{ height: "228px", width: "100%" }} src={item.imageCover} />
                     <Card.Body>
                         <Card.Title>
                             <div className="card-title">
-                               {item.title}
+                                {item.title}
                             </div>
                         </Card.Title>
                         <Card.Text>
--- src/redux/reducers/productsReducer.js
+++ src/redux/reducers/productsReducer.js
@@ -6,6 +6,7 @@ const inital = {
     oneProduct: [],
     productLike: [],
     deleteProducts: [],
+    updateProducts: [],
     loading: true,
 }
 
