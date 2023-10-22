2f3ea807a1d4a4638e189e91d88b21cfb27c05bd
MahmoudAliKhider <mahmoudkhider37@gmail.com>
2023-10-22 03:47:37 +0300

    add Authorization

--- src/hooks/useDeleteData.js
+++ src/hooks/useDeleteData.js
@@ -1,7 +1,10 @@
 import baseUrl from '../Api/baseURL'
 
 const useDeleteData = async (url, parmas) => {
-    const res = await baseUrl.delete(url, parmas);
+    const config = {
+        headers: { Authorization: `Bearer ${localStorage.getItem("token")}` }
+    }
+    const res = await baseUrl.delete(url, parmas, config);
     return res.data;
 }
 
--- src/hooks/useUpdateData.js
+++ src/hooks/useUpdateData.js
@@ -3,14 +3,18 @@ import baseUrl from '../Api/baseURL'
 
 const useInUpdateDataWithImage = async (url, parmas) => {
     const config = {
-        headers: { "Content-Type": "multipart/form-data" }
+        headers: { "Content-Type": "multipart/form-data", Authorization: `Bearer ${localStorage.getItem("token")}` }
     }
     const res = await baseUrl.put(url, parmas, config);
+    console.log(res.status)
     return res;
 }
 
 const useInsUpdateData = async (url, parmas) => {
-    const res = await baseUrl.put(url, parmas);
+    const config = {
+        headers: { Authorization: `Bearer ${localStorage.getItem("token")}` }
+    }
+    const res = await baseUrl.put(url, parmas, config);
     return res;
 }
 
